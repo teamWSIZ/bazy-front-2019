9c21c16821d92a720637571e9a4046aeceb64424
pmarecki <pmarecki@gmail.com>
2019-05-18 13:38:36 +0200

    x

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Employee} from "../model/employee";
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.less']
 })
 export class WelcomeComponent implements OnInit {
+  private employees: Employee[];  //tablica obiektów typu Employee
+  private welcomeMessage = 'Hello and welcome to employee app!';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
+  }
+
+  loadEmployees() {
+    const url = 'http://localhost:9999/employees';
+    this.http.get<Employee[]>(url)
+      .subscribe(res=>{
+        this.employees = res;
+      })
   }
 
 }
