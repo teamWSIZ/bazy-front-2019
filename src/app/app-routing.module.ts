3a8dd6628f3b3a2a2607036d8d6f8637630a2338
pmarecki <pmarecki@gmail.com>
2019-06-08 11:35:08 +0200

    Browse customers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import {ChatComponent} from './chat/chat.component';
+import {CustomerbrowserComponent} from "./customerbrowser/customerbrowser.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'chat', component: ChatComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'customers', component: CustomerbrowserComponent },
 ];
 
 @NgModule({
--- src/app/customerbrowser/customerbrowser.component.ts
+++ src/app/customerbrowser/customerbrowser.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from "../model/customer";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-customerbrowser',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customerbrowser.component.less']
 })
 export class CustomerbrowserComponent implements OnInit {
+  customers: Customer[];
+  url = 'http://localhost:3003/customers';
+  editedMessage: Customer = new Customer();
+  countries: string[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.loadCustomers();
   }
 
+  private loadCustomers() {
+    this.http.get<Customer[]>(this.url)
+      .subscribe(res => {
+        this.customers = res;
+        const cou = new Set();
+        for(let c of this.customers) {
+          cou.add(c.country);
+        }
+        this.countries = Array.from<string>(cou);
+      });
+  }
+
+  saveCustomer() {
+
+  }
 }
